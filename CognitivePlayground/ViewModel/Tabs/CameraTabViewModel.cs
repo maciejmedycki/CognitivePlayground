0521edaabcc5e058b8b14eac0b51bbb90483e0b3
maciej.medycki <maciej.medycki@systell.pl>
2018-10-09 16:25:21 +0200

    Add Emgu.CV and connecting to rtsp stream

--- CognitivePlayground/ViewModel/Tabs/CameraTabViewModel.cs
+++ CognitivePlayground/ViewModel/Tabs/CameraTabViewModel.cs
@@ -1,6 +1,11 @@
-﻿using GalaSoft.MvvmLight.Command;
+﻿using Emgu.CV;
+using GalaSoft.MvvmLight.Command;
 using log4net;
+using System;
+using System.Threading.Tasks;
 using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
 
 namespace CognitivePlayground.ViewModel.Tabs
 {
@@ -8,6 +13,8 @@ namespace CognitivePlayground.ViewModel.Tabs
     {
         public string _cameraAddress;
         private static readonly ILog _logger = LogManager.GetLogger(typeof(CameraTabViewModel));
+        private VideoCapture _capture;
+        private ImageSource _image;
         private ICommand _startCommand;
 
         public string CameraAddress
@@ -31,6 +38,19 @@ namespace CognitivePlayground.ViewModel.Tabs
             }
         }
 
+        public ImageSource Image
+        {
+            get
+            {
+                return _image;
+            }
+            set
+            {
+                _image = value;
+                RaisePropertyChanged(() => Image);
+            }
+        }
+
         public ICommand StartCommand
         {
             get
@@ -45,10 +65,29 @@ namespace CognitivePlayground.ViewModel.Tabs
 
         public override string Title { get; } = "Camera";
 
-        private void DoStart()
+        private async void DoStart()
         {
             _logger.Info($"DoStart() called for CameraAddress: \"{CameraAddress}\"");
-            //TODO: start capturing video from CameraAddress.
+            if (_capture != null)
+            {
+                _capture.Dispose();
+                _capture = null;
+            }
+            _capture = new VideoCapture(CameraAddress);
+            var fps = _capture.GetCaptureProperty(Emgu.CV.CvEnum.CapProp.Fps);
+            var interval = TimeSpan.FromSeconds(1 / fps);
+            while (true)
+            {
+                Mat m = new Mat();
+                _capture.Read(m);
+                //var m =_capture.QueryFrame();//old <v.3
+                if (!m.IsEmpty)
+                {
+                    Image = System.Windows.Interop.Imaging.CreateBitmapSourceFromHBitmap(m.Bitmap.GetHbitmap(), IntPtr.Zero, System.Windows.Int32Rect.Empty,
+                        BitmapSizeOptions.FromWidthAndHeight(m.Bitmap.Width, m.Bitmap.Height));
+                }
+                await Task.Delay(interval);//1000/fps capture.GetCapturePropoerty(CapProp.Fps)
+            }
         }
     }
 }
